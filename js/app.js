f8cc793c9a43f1a41a9ef2e040636cec5de646de
Oluchi Enemuo <oluchi.enemuo@yahoo.com>
2015-02-28 04:27:37 +0100

    updated changes to html, css and js files

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
-$(function(){
-  window.Sound = {
+var MySound = {
     searchSound: $('#term'),
     searchButton: $('#searchBtn'),
     searchValue : function() {
-      var searchInput = Sound.searchSound.val();
+      var searchInput = MySound.searchSound.val();
         console.log(searchInput);
-      //var searchTerm = searchInput
+      //var searchTerm = searchInput;
 
-      function  init() {
-        client_id = '3720e9819eec714050d69665601dad27';
-        
-      };
-    };
-  };
-  Sound.searchButton.click(Sound.searchValue());
-});
+      SC.initialize({
+        client_id: '3720e9819eec714050d69665601dad27'
+      });
+
+      SC.get('/tracks', { q: searchInput }, function(tracks) {
+        $(tracks).each(function(index, track) {
+          $('<li id="links"><a href=' + track.permalink_url +' target="_blank">' + track.title + '</a></li>').appendTo('#results');
+            $("ul").appendTo("#list");
+            
+            var track_url = track.permalink_url;
+            SC.oEmbed(track_url, {auto_play: true}, function(oEmbed){
+              console.log('oEmbed response: ' + oEmbed);
+              $('#player').html(track.title); SC.oEmbed(track.permalink_url,
+                 document.getElementById('player')); 
+            });
+        });
+        SC.get('/tracks/1', function(track, error) {
+          if (q === "") { alert('Error: ' + error.message); }
+        });
+      });
+
+
+      
+    },
+
+    init: function () {
+      $('#searchBtn').click(function() {
+        $('#results').empty();
+          MySound.searchValue();
+          //console.log('Hello');
+      });
+    }
+    
+}
+MySound.init();
